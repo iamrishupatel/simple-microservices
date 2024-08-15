1f3eedb06627a432b85d5b37fe03d37ea5abfb80
iamrishupatel <rishupatel04@gmail.com>
2024-08-15 14:51:22 +0530

    update post create endpoint in post srv

--- posts/index.js
+++ posts/index.js
@@ -1,75 +1,74 @@
-const app = require('express')();
-const { randomBytes } = require('crypto');
-const bodyParser = require('body-parser');
-const cors = require('cors');
-const axios = require('axios');
-
+const app = require("express")();
+const { randomBytes } = require("crypto");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const axios = require("axios");
 
 const DB = {};
 
-
-
 app.use(bodyParser.json());
-app.use(cors({
-  origin: 'http://localhost:3000'
-}));
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+  })
+);
 
 app.get("/", (req, res) => {
-  res.send("<h1>Welcome to post service</h1>")
-})
+  res.send("<h1>Welcome to post service</h1>");
+});
 
-app.post("/post", async (req, res) => {
-  const id = randomBytes(4).toString('hex');
+app.post("/post/create", async (req, res) => {
+  const id = randomBytes(4).toString("hex");
   const { title } = req.body;
 
   DB[id] = {
-    id, title
+    id,
+    title,
   };
 
   try {
-    await axios.post('http://events-clusterip-srv:8005/events', {
-      type: 'postCreated',
+    await axios.post("http://events-clusterip-srv:8005/events", {
+      type: "postCreated",
       data: {
-        id, title
-      }
-    })
+        id,
+        title,
+      },
+    });
   } catch (e) {
-    return res.send(e).end()
-
+    return res.send(e).end();
   }
-
 
   return res
     .status(201)
     .json({
-      "message": "post created successfully",
-      "data": {
-        id: DB[id]
-      }
+      message: "post created successfully",
+      data: {
+        id: DB[id],
+      },
     })
     .end();
-
 });
 
-app.get('/post', (req, res) => {
-  res.status(200).json({
-    data: DB
-  }).end()
-})
+app.get("/post", (req, res) => {
+  res
+    .status(200)
+    .json({
+      data: DB,
+    })
+    .end();
+});
 
-
-app.get('/db', (req, res) => {
+app.get("/db", (req, res) => {
   res.status(200).send(`
   <p>Post service DB</p>
   <pre>${JSON.stringify(DB, null, "\t")}</pre>
-  `)
-})
+  `);
+});
 
-
-app.post('/events', (req, res) => {
+app.post("/events", (req, res) => {
   res.send({}).end();
-})
+});
 
 app.listen(8001, () => {
-  console.log("listening on http://localhost:8001")
-})
+  console.log("listening on http://localhost:8001");
+});
--- query/index.js
+++ query/index.js
@@ -1,78 +1,75 @@
-const express = require('express');;
-const cors = require('cors');
-const bodyParser = require('body-parser');
-const axios = require('axios')
+const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const axios = require("axios");
 
 const DB = {};
 
 const app = express();
 
 app.use(bodyParser.json());
-app.use(cors({
-  origin: 'http://localhost:3000'
-}))
-
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+  })
+);
 
 function handleEvents(type, data) {
   switch (type) {
-    case 'commentCreated':
-      const commentData = data
+    case "commentCreated":
+      const commentData = data;
       DB[commentData.postId].comments.push({
         id: commentData.id,
         content: commentData.content,
-        status: commentData.status
-      })
-      return 
+        status: commentData.status,
+      });
+      return;
 
-
-    case 'postCreated':
+    case "postCreated":
       const postData = data;
       DB[postData.id] = {
         id: postData.id,
         title: postData.title,
-        comments: []
-      }
-      return 
+        comments: [],
+      };
+      return;
 
-    case 'commentUpdated':
+    case "commentUpdated":
       const { postId, id, status, content } = data;
       const post = DB[postId];
       const comment = post.comments.find(comment => comment.id === id);
       comment.status = status;
-      comment.content = content
+      comment.content = content;
 
-      return 
+      return;
     default:
-      return
+      return;
   }
 }
 
-app.post('/events', (req, res) => {
-  const { type, data } = req.body
+app.post("/events", (req, res) => {
+  const { type, data } = req.body;
   handleEvents(type, data);
   return res.status(201).send({});
+});
 
-
-})
-
-app.get('/posts', (req, res) => {
+app.get("/posts", (req, res) => {
   res.status(200).json({
-    data: DB
-  })
-})
+    data: DB,
+  });
+});
 
-app.get('/db', (req, res) => {
+app.get("/db", (req, res) => {
   res.status(200).send(`
   <p>Query service DB</p>
   <pre>${JSON.stringify(DB, null, "\t")}</pre>
-  `)
-})
-
+  `);
+});
 
 app.listen(8003, async () => {
-  console.log('listening on http://localhost:8003');
-  const { data } = await axios.get('http://events-clusterip-srv:8005/events')
-  data.forEach((event) => {
-    handleEvents(event.type, event.data)
-  })
-})
+  console.log("listening on http://localhost:8003");
+  const { data } = await axios.get("http://events-clusterip-srv:8005/events");
+  data.forEach(event => {
+    handleEvents(event.type, event.data);
+  });
+});
